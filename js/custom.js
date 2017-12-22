a3bdc0bed4c6c219d76032a4f0b7458fbc701005
Yuber Millan <yub3r@hotmail.com>
2017-12-22 11:13:51 -0400

    Seccion WORK Galeria

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,18 @@ $(document).ready(function () {
     new WOW().init();
 
 });            
+
+/*===============================================
+                        Work
+================================================*/
+$(document).ready(function(){
+
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+
+});
