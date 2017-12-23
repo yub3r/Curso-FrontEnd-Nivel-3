a9bad779aee1c48418f2d38688a58939267be86f
Yuber Millan <yub3r@hotmail.com>
2017-12-23 10:44:12 -0400

    Seccion Princing Barnav Footer Contact

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 /*===============================================
                                         Services
-                ================================================*/
+================================================*/
 $(document).ready(function() {
 
     //Animate Scroll
@@ -49,6 +49,67 @@ $(document).ready(function() {
         smartSpeed: 700,
         loop: true,
         autoplayHoverPause: true
+    });
+
+});
+
+/*===============================================
+                 Stats
+================================================*/
+$(document).ready(function() {
+
+    $(".counter").counterUp({
+        delay: 10,
+        time: 3000
+    });
+
+});
+
+/*===============================================
+                         Clients
+ ================================================*/
+$(document).ready(function() {
+
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+
+});
+
+/*===============================================
+                        Smooth-Scroll
+================================================*/
+
+$("a.smooth-scroll").click(function(evento) {
+
+    evento.preventDefault();
+    var section = $(this).attr("href");
+
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+
+}, 1250);
+
+
+/*===============================================
+                        Button Top
+================================================*/
+
+$(function() {
+
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() < 50) {
+            $("#back-to-top").fadeOut();
+        } else {
+            $("#back-to-top").fadeIn();
+        }
+
     });
 
 });
